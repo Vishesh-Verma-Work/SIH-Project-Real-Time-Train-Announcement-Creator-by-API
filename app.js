21a194506f31fe797ff895d1ca1ea0cced0a51ac
Vishesh-Verma-Work <visheshverma.workin@gmail.com>
2023-11-03 00:01:56 -0700

    Added arrival and depart time data

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ btn.addEventListener("click", async () => {
     };
 
     try {
-        console.log("Not working :(");
+        // console.log("Not working :(");
         const response = await fetch(url, options);
 
         const result = await response.json();
@@ -30,6 +30,8 @@ btn.addEventListener("click", async () => {
                 console.log("Train Number : ", result[i].train_num);
                 console.log("From : ", result[i].train_from);
                 console.log("To : ", result[i].train_to);
+                console.log("ar : ", result[i].data.arriveTime);
+                console.log("dep : ", result[i].data.departTime);
                 console.log("");
 
                 const jsname = document.querySelector(`.jsname${i}`);
@@ -40,6 +42,10 @@ btn.addEventListener("click", async () => {
                 jsfrom.innerText = result[i].train_from;
                 const jsto = document.querySelector(`.jsto${i}`);
                 jsto.innerText = result[i].train_to;
+                const jsarv = document.querySelector(`.jsarv${i}`);
+                jsarv.innerText = result[i].data.arriveTime;
+                const jsdep = document.querySelector(`.jsdep${i}`);
+                jsdep.innerText = result[i].data.departTime;
 
             }
             catch (e) {
